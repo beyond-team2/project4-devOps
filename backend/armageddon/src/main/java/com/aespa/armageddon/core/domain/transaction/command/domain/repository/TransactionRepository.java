0993fa98b8534ab7439b0d624ec61a756258fd61
hatahng <hathang16@naver.com>
2026-01-09 17:42:47 +0900

    feat(transaction): TransactionRepository save 메서드 추가

--- backend/armageddon/src/main/java/com/aespa/armageddon/core/domain/transaction/command/domain/repository/TransactionRepository.java
+++ backend/armageddon/src/main/java/com/aespa/armageddon/core/domain/transaction/command/domain/repository/TransactionRepository.java
@@ -1,5 +1,8 @@
 package com.aespa.armageddon.core.domain.transaction.command.domain.repository;
 
+import com.aespa.armageddon.core.domain.transaction.command.domain.aggregate.Transaction;
+
 public interface TransactionRepository {
 
+    Transaction save(Transaction transaction);
 }
