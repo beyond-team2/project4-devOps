2183bb1783528997baf8a982329b7fd9a38e0595
whatimeisitnow <jayjay.hi.77@gmail.com>
2026-01-09 17:27:12 +0900

    feat:í(Statistics):통계관련 로직 추가

---
+++ backend/armageddon/src/main/java/com/aespa/armageddon/core/domain/cashflow/controller/StatisticsController.java
@@ -0,0 +1,23 @@
+package com.aespa.armageddon.core.domain.cashflow.controller;
+
+import com.aespa.armageddon.core.domain.cashflow.dto.SummaryStatisticsResponse;
+import com.aespa.armageddon.core.domain.cashflow.service.StatisticsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+@RestController
+@RequestMapping("/api/statistics")
+@RequiredArgsConstructor
+public class StatisticsController {
+
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/summary")
+    public SummaryStatisticsResponse getSummary(
+
+    )
+}
+*/
